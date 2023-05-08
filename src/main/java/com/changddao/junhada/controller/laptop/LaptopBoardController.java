6b56aecb0586b2785102aeb48984d60468b1fdd0
Changhogod <younch8342@naver.com>
2023-05-09 01:34:40 +0900

    laptopBoardView Page 처리 및 Board 작성하기 구상중 (laptopBoard)에서 글 작성할때 이미지를 여러개 추가할 수 있으므로 Back에서 같이 처리하는 것이 이상적이라 생각함 board가 저장될 때 ->cascade 형식으로 이미지들까지 저장되도록 설계하자

--- src/main/java/com/changddao/junhada/controller/laptop/LaptopBoardController.java
+++ src/main/java/com/changddao/junhada/controller/laptop/LaptopBoardController.java
@@ -1,17 +1,43 @@
 package com.changddao.junhada.controller.laptop;
 
+import com.changddao.junhada.controller.MsgAlert;
+import com.changddao.junhada.entity.LaptopBoardDto;
+import com.changddao.junhada.repository.laptop.LaptopBoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class LaptopBoardController {
+    private final LaptopBoardRepository laptopBoardRepository;
     @GetMapping("/laptop/board")
-    public String laptopBoardsList(Model model, Pageable pageable) {
+    public String laptopBoardsList(Model model,@PageableDefault(size=2)Pageable pageable) {
+        Page<LaptopBoardDto> laptopBoards = laptopBoardRepository.laptopBoardPage(pageable);
+        int startPage = Math.max(1, laptopBoards.getPageable().getPageNumber()-4);
+        int endPage = Math.min(laptopBoards.getTotalPages(), laptopBoards.getPageable().getPageNumber()+4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("laptopBoards", laptopBoards);
+
         return "boards/laptopBoardView";
+    }
+    @GetMapping("/laptop/board/write")
+    public String writeLaptopBoard(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if (session == null) {
+            model.addAttribute("data", new MsgAlert("로그인 후 이용가능합니다.", "/"));
+        } else {
+            return "boards/laptopBoardForm";
+        }
+        return "message";
     }
 
 }
