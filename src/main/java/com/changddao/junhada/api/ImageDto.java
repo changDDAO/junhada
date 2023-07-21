9e1454a647d1da5791272ac4bb7986c4b3ee3e60
Changhogod <younch8342@naver.com>
2023-07-21 18:50:43 +0900

    connect with flutter get_x app

--- src/main/java/com/changddao/junhada/api/ImageDto.java
+++ src/main/java/com/changddao/junhada/api/ImageDto.java
@@ -7,9 +7,11 @@ import lombok.Getter;
 public class ImageDto {
     Long id;
     String savedPath;
+    String imageLink;
 
     public ImageDto(LaptopFile laptopFile) {
         this.id = laptopFile.getId();
         this.savedPath = laptopFile.getSavedPath();
+        this.imageLink = "ddaowifi.iptime.org:8080/api/images/"+String.valueOf(id);
     }
 }
